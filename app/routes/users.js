688dc94061d23e27fece818561cb6c3772b42980
ByeolAhn <bahn1@my.centennialcollege.ca>
2023-10-14 21:32:58 -0400

    added api on route

--- app/routes/users.js
+++ app/routes/users.js
@@ -4,10 +4,10 @@
 module.exports = (router) => {
   const productController = require('../controller/products.controller');
 
-  router.get('/products', productController.getAll);
-  router.get('/products/:id', productController.productByID);
-  router.post('/products', productController.addNew);
-  router.put('/products/:id', productController.updateById);
-  router.delete('/products/:id', productController.removeById);
+  router.get('/api/products', productController.getAll);
+  router.get('/api/products/:id', productController.productByID);
+  router.post('/api/products', productController.addNew);
+  router.put('/api/products/:id', productController.updateById);
+  router.delete('/api/products/:id', productController.removeById);
 
 }
